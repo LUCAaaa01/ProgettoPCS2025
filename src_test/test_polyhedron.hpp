d82546a17770e5e018891c8cfa568d27fc4fb3b7
Nicolò Rizzo <nicorizzo2004@gmail.com>
2025-05-26 18:08:14 +0200

    Update test_polyhedron.hpp

--- src_test/test_polyhedron.hpp
+++ src_test/test_polyhedron.hpp
@@ -1,1 +1,10 @@
+#pragma once
 
+#include <gtest/gtest.h>
+#include "PolyhedronCollection.hpp"
+#include "face.hpp"
+#include "Eigen/Eigen"
+
+class PolyhedronTest : public ::testing::Test {
+protected:
+  PolyhedronCollection p;
